ab6ce219cf27b74f0815443f99554a739d24b5a1
Anand <vadlakondaanand45@gmail.com>
2024-01-24 14:31:41 +0530

    Tic Tac Toe

---
+++ src/models/Bot.java
@@ -0,0 +1,30 @@
+package models;
+
+import Strategy.EasyBotPlayingStrategy;
+import Strategy.BotPlayingStrategy;
+public class Bot extends Player {
+    private BotDifficultyLevel botDifficultyLevel;
+    private BotPlayingStrategy botPlayingStrategy;
+
+    public Bot(char c, String name, BotDifficultyLevel botDifficultyLevel) {
+        super(c, name, PlayerType.BOT);
+        this.botDifficultyLevel = botDifficultyLevel;
+        //TODO: Better way is via factory based on the bot difficulty level
+        this.botPlayingStrategy = new EasyBotPlayingStrategy();
+
+
+    }
+
+    public BotDifficultyLevel getBotDifficultyLevel() {
+        return botDifficultyLevel;
+    }
+
+    public void setBotDifficultyLevel(BotDifficultyLevel botDifficultyLevel) {
+        this.botDifficultyLevel = botDifficultyLevel;
+    }
+    public Move decideMove(Board board) {
+        return botPlayingStrategy.decideMove(this, board);
+    }
+
+
+}
